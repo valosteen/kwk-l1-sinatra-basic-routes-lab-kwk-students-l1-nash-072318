6c48677f7a5cbd2165c404e2d51e8180679a663c
valosteen <val_osteen@oneidaschool.org>
2018-07-31 18:45:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+
+get '/'do
+  "Hello world"
+end
+
+get '/name'do
+  "My name is _"
+end
+
+get '/hometwon'do
+  "My hometown is _"
+end
+
+get '/favorite-song'do
+  "My favorite song is _"
+end
+
 end
